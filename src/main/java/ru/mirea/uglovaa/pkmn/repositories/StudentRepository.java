25d1160c0cef1880bbc80a27268e1fd3347c34b3
UtwoA <loskon64@gmail.com>
2025-01-08 22:47:00 +0400

    v6 recovery

--- src/main/java/ru/mirea/uglovaa/pkmn/repositories/StudentRepository.java
+++ src/main/java/ru/mirea/uglovaa/pkmn/repositories/StudentRepository.java
@@ -1,4 +1,14 @@
 package ru.mirea.uglovaa.pkmn.repositories;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.mirea.uglovaa.pkmn.entities.StudentEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
+    List<StudentEntity> findByGroup(String group);
+
+    Optional<StudentEntity> findByFirstNameAndSurNameAndFamilyName(String firstName, String surName, String familyName);
 }
